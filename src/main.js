e8208d03a25d03b3349ec1f6fc0987afdbe75052
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-02 11:30:28 -0400

    emptying

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 
+import '@/assets/css/style.css'
+
 createApp(App).use(router).use(store).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DefaultLayout from './../layouts/DefaultLayout'
+import HomeView from './../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+	{
+		path: '/',
+		component: DefaultLayout,
+		children: [
+			{ path: '', name: 'index', component: HomeView }
+		]
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes
 })
 
 export default router
