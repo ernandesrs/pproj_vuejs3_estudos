24784ee04c39407f3a592e00d2fc43e87a8b07ee
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-03 13:34:47 -0400

    Populando navegação do elemento do sidebar dinamicamente. Item de navegação dinãmica para 'RouterLink' e 'a'

--- src/components/Layout/MainSidebar/SidebarNav.vue
+++ src/components/Layout/MainSidebar/SidebarNav.vue
@@ -1,6 +1,7 @@
 <template>
     <nav class="flex flex-col">
-        <SidebarNavItem v-for="item in navItems" v-bind:key="item" :text="item.text" />
+        <SidebarNavItem v-for="item in navItems" v-bind:key="item" :text="item.text"
+            :to="item.to" :href="item.href" :title="item.title" :target="item.target" />
     </nav>
 </template>
 
--- src/components/Layout/MainSidebar/SidebarNavItem.vue
+++ src/components/Layout/MainSidebar/SidebarNavItem.vue
@@ -1,6 +1,6 @@
 <template>
-    <a
-        class="block px-2 py-2 mb-1 text-gray-400 hover:bg-gray-800 hover:pl-4 duration-300 hover:duration-300">
+    <Component :is="tag" :to="to" :href="href" :title="title" :target="target"
+        class="block px-2 py-2 mb-1 text-gray-400 hover:bg-gray-800 hover:pl-4 duration-300 hover:duration-300 cursor-pointer">
         <div class="flex items-center">
             <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -12,7 +12,7 @@
                 {{ text }}
             </span>
         </div>
-    </a>
+    </Component>
 </template>
 
 <script>
@@ -23,7 +23,39 @@ export default {
         text: {
             type: String,
             default: 'Nav item'
+        },
+
+        to: {
+            type: [Object, String],
+            default: undefined
+        },
+
+        href: {
+            type: String,
+            default: undefined
+        },
+
+        title: {
+            type: String,
+            default: null,
+        },
+
+        target: {
+            type: String,
+            default: '_self'
         }
+    },
+
+    computed: {
+        tag() {
+            if (typeof this.to != 'undefined')
+                return 'RouterLink';
+
+            return 'a';
+        }
+    },
+
+    methods: {
     }
 };
 </script>
